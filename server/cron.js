4521f51c1e3a9a6270c6842902c4e91c8c2fd85d
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-01-30 23:26:10 +0400

    Updating cron file to refresh pokemon data every day at 3am

--- server/cron.js
+++ server/cron.js
@@ -1,16 +1,21 @@
 const cron = require('node-cron');
 const axios = require('axios');
-const Pokemon = require('./models/Pokemon'); // Example model
+import Pokemon from "/models/Pokemon.js";
 
-// Schedule a task to run every hour
-cron.schedule('0 * * * *', async () => {
+// Schedule a task to run every day 3 am
+cron.schedule('0 0 3 * *', async () => {
     console.log('Fetching latest Pokémon data...');
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const limit = 150;
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
     const data = response.data.results;
 
     // Save data to the database
     data.forEach(async (pokemon) => {
-        await Pokemon.findOneAndUpdate({ name: pokemon.name }, pokemon, { upsert: true });
+        await Pokemon.findOneAndUpdate(
+            { name: pokemon.name },
+            pokemon,
+            { upsert: true }
+        );
     });
     console.log('Pokémon data updated successfully.');
 });
