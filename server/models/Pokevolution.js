f97ac8392f67b4171485ae83113741bd45473713
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-01-31 07:50:15 +0400

    bug fix for string column

--- server/models/Pokevolution.js
+++ server/models/Pokevolution.js
@@ -1,4 +1,4 @@
-import {DataTypes,literal} from "sequelize";
+import {DataTypes} from "sequelize";
 import sequelize from "../config/conn.js";
 import Pokemon from "./Pokemon.js";
 
@@ -7,7 +7,7 @@ const Pokevolution = sequelize.define("Pokevolution",{
     pokechain_id: { type: DataTypes.INTEGER,  allowNull: false },                                                   //evolution chain id from pokeAPI
     position:     { type: DataTypes.INTEGER,  allowNull: false,       defaultValue: 1 },                            //position of pokemon in evolution chain (default value 1)
     pokemon_id:   { type: DataTypes.INTEGER,  allowNull: false,       references: { model: Pokemon, key: 'id' } },  //id from pokemon model
-    method:       { type: DataTypes.STRING,   allowNull: false,       defaultValue: literal('level-up') },      //unless specified, this takes default value as "level-up"
+    method:       { type: DataTypes.STRING,   allowNull: false,       defaultValue: 'level-up' },      //unless specified, this takes default value as "level-up"
     item:         { type: DataTypes.STRING,   allowNull: true },                                                    //item used for evolution method, stays null by default
 });
 
