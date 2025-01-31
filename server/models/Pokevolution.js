8c47023cdb4ecd5da64f24a0f844e8290fc347cc
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-01-31 07:48:16 +0400

    1. added validation 2. added item used for evolution method 3. added comments

--- server/models/Pokevolution.js
+++ server/models/Pokevolution.js
@@ -1,13 +1,14 @@
-import { DataTypes } from "sequelize";
+import {DataTypes,literal} from "sequelize";
 import sequelize from "../config/conn.js";
 import Pokemon from "./Pokemon.js";
 
 const Pokevolution = sequelize.define("Pokevolution",{
     id:           { type: DataTypes.INTEGER,  autoIncrement: true,    primaryKey: true },
-    pokechain_id: { type: DataTypes.INTEGER,  allowNull: false },
-    position:     { type: DataTypes.INTEGER,  allowNull: false },
-    pokemon_id:   { type: DataTypes.INTEGER,  allowNull: false,       references: { model: Pokemon, key: 'id' } },
-    method:       { type: DataTypes.STRING,   allowNull: false },
+    pokechain_id: { type: DataTypes.INTEGER,  allowNull: false },                                                   //evolution chain id from pokeAPI
+    position:     { type: DataTypes.INTEGER,  allowNull: false,       defaultValue: 1 },                            //position of pokemon in evolution chain (default value 1)
+    pokemon_id:   { type: DataTypes.INTEGER,  allowNull: false,       references: { model: Pokemon, key: 'id' } },  //id from pokemon model
+    method:       { type: DataTypes.STRING,   allowNull: false,       defaultValue: literal('level-up') },      //unless specified, this takes default value as "level-up"
+    item:         { type: DataTypes.STRING,   allowNull: true },                                                    //item used for evolution method, stays null by default
 });
 
 Pokevolution.belongsTo(Pokemon, { foreignKey: 'pokemon_id' });
