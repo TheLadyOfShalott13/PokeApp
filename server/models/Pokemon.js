1c1475918ff2150712d2430ccf6f09643655f5b9
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-01-31 07:39:00 +0400

    adding pokechain ID to pokemon model to link with pokevolution model. Pokechain ID here is same as the original PokeAPI's evolution_id

--- server/models/Pokemon.js
+++ server/models/Pokemon.js
@@ -2,12 +2,13 @@ import {DataTypes} from "sequelize";
 import sequelize from "../config/conn.js";
 
 const Pokemon = sequelize.define("Pokemon",{
-        id:         { type: DataTypes.INTEGER,  autoIncrement: true,    primaryKey: true },
-        name:       { type: DataTypes.STRING,   allowNull: false },
-        identifier: { type: DataTypes.INTEGER,  allowNull: false },
-        slug:       { type: DataTypes.STRING,   allowNull: false },
-        image_path: { type: DataTypes.DATE,     allowNull: false },
-        poketypes:  { type: DataTypes.STRING,   allowNull: false }      //instead of a separate table, keep multiple IDs as a string separated with commas
+        id:             { type: DataTypes.INTEGER,  autoIncrement: true,    primaryKey: true },
+        name:           { type: DataTypes.STRING,   allowNull: false },
+        identifier:     { type: DataTypes.INTEGER,  allowNull: false },
+        slug:           { type: DataTypes.STRING,   allowNull: false },
+        image_path:     { type: DataTypes.DATE,     allowNull: false },
+        poketypes:      { type: DataTypes.STRING,   allowNull: false },      //instead of a separate table, keep multiple IDs as a string separated with commas
+        pokechain_id:   { type: DataTypes.INTEGER,  allowNull: false },      //chain_id from pokeAPI
     });
 
 export default Pokemon;
