2e284e82244691c1ff8e920535290fb6e4744079
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-02-04 20:45:01 +0400

    Fix logical error in pokemon evolution model. Ways will contain number of ways a pokemon can evolve to the current state

--- server/models/Pokevolution.js
+++ server/models/Pokevolution.js
@@ -7,8 +7,7 @@ const Pokevolution = sequelize.define("Pokevolution",{
     pokechain_id: { type: DataTypes.INTEGER,  allowNull: false },                                                   //evolution chain id from pokeAPI
     position:     { type: DataTypes.INTEGER,  allowNull: false,       defaultValue: 1 },                            //position of pokemon in evolution chain (default value 1)
     pokemon_id:   { type: DataTypes.INTEGER,  allowNull: false,       references: { model: Pokemon, key: 'id' } },  //id from pokemon model
-    method:       { type: DataTypes.STRING,   allowNull: false,       defaultValue: 'level-up' },      //unless specified, this takes default value as "level-up"
-    item:         { type: DataTypes.STRING,   allowNull: true },                                                    //item used for evolution method, stays null by default
+    ways:         { type: DataTypes.INTEGER,  allowNull: false,       defaultValue: 0 },                   //
 });
 
 Pokevolution.belongsTo(Pokemon, { foreignKey: 'pokemon_id' });
