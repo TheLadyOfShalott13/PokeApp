513a9763a3c1847d37929e89c7e29789230d19a5
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-02-08 06:27:00 +0400

    removing redundant field

--- server/models/Favorites.js
+++ server/models/Favorites.js
@@ -5,8 +5,7 @@ import User from "./User.js";
 const Favorites = sequelize.define("Favorites",{
     id:         { type: DataTypes.INTEGER,  autoIncrement: true,    primaryKey: true },
     poke_id:    { type: DataTypes.INTEGER,   allowNull: false },
-    user_id:    { type: DataTypes.INTEGER,   allowNull: false,       references: { model: User, key: 'id' } },
-    added_on:   { type: DataTypes.DATE,     allowNull: false,       defaultValue: literal('CURRENT_TIMESTAMP') },
+    user_id:    { type: DataTypes.INTEGER,   allowNull: false,       references: { model: User, key: 'id' } }
 },
 {
     indexes: [{
