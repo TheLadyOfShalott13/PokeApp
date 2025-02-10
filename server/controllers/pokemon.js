3c8391da0498017925470c78a8508acb8d53f078
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-02-10 17:18:22 +0400

    adding type info and colours

--- server/controllers/pokemon.js
+++ server/controllers/pokemon.js
@@ -1,4 +1,25 @@
 import Pokemon from "../models/Pokemon.js";
+import Poketype from "../models/Poketype.js";
+const colours = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+};
 
 
 //================= Add a pokemon =======================//
@@ -18,8 +39,25 @@ export const addPokemon = async (req, res, next) => {
 //================= List all pokemons =======================//
 export const getAllPokemon = async (req, res, next) => {
     try {
+        const pokemonTypesList = await Poketype.findAll();
+        let pokemonTypes = {}
+        pokemonTypesList.map(type => {
+            pokemonTypes[type.id] = type.name;
+        })
+
         const pokemonList = await Pokemon.findAll();
-        res.status(200).json(pokemonList);
+
+        const modifiedPokemons = pokemonList.map((pokemon) => {
+            const types = pokemon.poketypes.split(",");
+            const pokemonType = types.map((type) => ({
+                id: type,
+                name: pokemonTypes[type],
+                colour: colours[pokemonTypes[type]],
+            }));
+            return { ...pokemon.get({plain: true}), type: pokemonType };
+        });
+
+        res.status(200).json(modifiedPokemons);
     } catch (err) {
         next(err)
     }
@@ -29,8 +67,27 @@ export const getAllPokemon = async (req, res, next) => {
 //================= Get One pokemon =======================//
 export const getOnePokemon = async (req, res, next) => {
     try {
+
+        const pokemonTypesList = await Poketype.findAll();
+        let pokemonTypes = {}
+        pokemonTypesList.map(type => {
+            pokemonTypes[type.id] = type.name;
+        })
+
         const onePokemon = await Pokemon.findAll({ where: { id: req.params.id } });
-        res.status(200).json(onePokemon);
+
+        const modifiedPokemon = onePokemon.map((pokemon) => {
+            const types = pokemon.poketypes.split(",");
+            const pokemonType = types.map((type) => ({
+                id: type,
+                name: pokemonTypes[type],
+                colour: colours[pokemonTypes[type]],
+            }));
+            return { ...pokemon.get({plain: true}), type: pokemonType };
+        });
+
+        res.status(200).json(modifiedPokemon);
+
     } catch (err) {
         next(err)
     }
